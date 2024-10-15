f8bfd97f7f8c53dd5687a483314a4efe626771a0
gallardovalen21 <62915335+gallardovalen21@users.noreply.github.com>
2024-10-15 17:55:30 -0300

    Update PruebaRepository.java

--- src/main/java/grupo107/demo/repositories/PruebaRepository.java
+++ src/main/java/grupo107/demo/repositories/PruebaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+/*https://labsys.frc.utn.edu.ar/gitlab/backend-app/alumnos/proyectos-clase/3k8/sping-service-web/-/blob/main/src/main/java/ar/edu/utnfrc/sping_service_web/services/ClientServiceImpl.java?ref_type=heads*/
 @Repository
 public interface PruebaRepository extends JpaRepository<Prueba, Integer> {
     List<Prueba> findByVehiculoId(int vehiculoId);
